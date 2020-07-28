7263e5d0779e2412a028e4d405b4f4e09d2ff356
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-29 00:32:13 +0530

    Location of db dump in directory changed

--- lms.sql
+++ lms.sql
@@ -35,263 +35,6 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: classes; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.classes (
-    class_id integer NOT NULL,
-    class_name character varying(100) NOT NULL,
-    teacher_id integer NOT NULL,
-    number_of_students integer DEFAULT 0,
-    time_of_class character varying(20),
-    days_of_class character varying(10)[],
-    useful_resources text[],
-    teacher_name character varying(100) NOT NULL
-);
-
-
-ALTER TABLE public.classes OWNER TO postgres;
-
---
--- Name: classes_class_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.classes_class_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.classes_class_id_seq OWNER TO postgres;
-
---
--- Name: classes_class_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.classes_class_id_seq OWNED BY public.classes.class_id;
-
-
---
--- Name: user_class_map; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.user_class_map (
-    user_id integer NOT NULL,
-    class_id integer NOT NULL,
-    date_enrolled character varying(10),
-    date_created character varying(10),
-    type character varying(10) NOT NULL,
-    user_name character varying(100) NOT NULL,
-    class_name character varying(100) NOT NULL
-);
-
-
-ALTER TABLE public.user_class_map OWNER TO postgres;
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.users (
-    user_id integer NOT NULL,
-    email character varying(50),
-    phone character varying(15) NOT NULL,
-    password character varying(100) NOT NULL,
-    date_of_birth character varying(10),
-    city character varying(20),
-    state character varying(20),
-    name character varying(100) NOT NULL
-);
-
-
-ALTER TABLE public.users OWNER TO postgres;
-
---
--- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.users_user_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.users_user_id_seq OWNER TO postgres;
-
---
--- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
-
-
---
--- Name: classes class_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.classes ALTER COLUMN class_id SET DEFAULT nextval('public.classes_class_id_seq'::regclass);
-
-
---
--- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
-
-
---
--- Data for Name: classes; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.classes (class_id, class_name, teacher_id, number_of_students, time_of_class, days_of_class, useful_resources, teacher_name) FROM stdin;
-\.
-
-
---
--- Data for Name: user_class_map; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.user_class_map (user_id, class_id, date_enrolled, date_created, type, user_name, class_name) FROM stdin;
-\.
-
-
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.users (user_id, email, phone, password, date_of_birth, city, state, name) FROM stdin;
-\.
-
-
---
--- Name: classes_class_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.classes_class_id_seq', 1, false);
-
-
---
--- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
-
-
---
--- Name: classes classes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.classes
-    ADD CONSTRAINT classes_pkey PRIMARY KEY (class_id);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
-
-
---
--- Name: class_details_index; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE INDEX class_details_index ON public.classes USING btree (class_id);
-
-
---
--- Name: class_map_index; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE INDEX class_map_index ON public.user_class_map USING btree (class_id);
-
-
---
--- Name: user_details_index; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE INDEX user_details_index ON public.users USING btree (user_id);
-
-
---
--- Name: user_map_index; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE INDEX user_map_index ON public.user_class_map USING btree (user_id);
-
-
---
--- Name: classes classes_teacher_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.classes
-    ADD CONSTRAINT classes_teacher_id_fkey FOREIGN KEY (teacher_id) REFERENCES public.users(user_id);
-
-
---
--- Name: user_class_map user_class_map_class_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.user_class_map
-    ADD CONSTRAINT user_class_map_class_id_fkey FOREIGN KEY (class_id) REFERENCES public.classes(class_id);
-
-
---
--- Name: user_class_map user_class_map_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.user_class_map
-    ADD CONSTRAINT user_class_map_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id);
-
-
---
--- PostgreSQL database dump complete
---
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 10.12 (Ubuntu 10.12-0ubuntu0.18.04.1)
--- Dumped by pg_dump version 10.12 (Ubuntu 10.12-0ubuntu0.18.04.1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
 -- Name: classrooms; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -331,6 +74,20 @@ ALTER TABLE public.classes_class_id_seq OWNER TO postgres;
 
 ALTER SEQUENCE public.classes_class_id_seq OWNED BY public.classrooms.classroom_id;
 
+
+--
+-- Name: roles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.roles (
+    id integer NOT NULL,
+    name character varying(255),
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL
+);
+
+
+ALTER TABLE public.roles OWNER TO postgres;
 
 --
 -- Name: session; Type: TABLE; Schema: public; Owner: postgres
@@ -419,7 +176,18 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 --
 
 COPY public.classrooms (classroom_id, classroom_name, teacher_id, number_of_students, timings, days, useful_resources, teacher_name, subject) FROM stdin;
-2	My Class	5	10	9am to 10am	{Monday}	{"Class Notes: NCERT"}	teacher 1	Physics
+7	New Class	5	16	9am to 10am	{Monday}	{"Class Notes: NCERT"}	teacher 1	Physics
+2	Faraz Classes	5	4	9am to 11am	{Monday}	{"Class Notes: NCERT","Notes: Class Notes"}	teacher 1	Maths
+8	First Class	5	26	9am to 10am	{Monday}	{"Class Notes: NCERT","Notes: Class Notes","Extra: Other resources","Favourite: My favourite resourcce"}	teacher 1	Chemistry
+14	Computer Class	5	4	6pm to 12pm	{Monday,Wednesday," Friday"," Sunday"}	{"Notes: Class Notes"}	teacher 1	Computer
+\.
+
+
+--
+-- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.roles (id, name, "createdAt", "updatedAt") FROM stdin;
 \.
 
 
@@ -428,7 +196,7 @@ COPY public.classrooms (classroom_id, classroom_name, teacher_id, number_of_stud
 --
 
 COPY public.session (sid, sess, expire) FROM stdin;
-f9UJDkuBnAQ_7gE7lt9Y_a11OIaBman6	{"cookie":{"originalMaxAge":1296000000,"expires":"2020-08-09T15:44:39.479Z","httpOnly":true,"path":"/","sameSite":true},"name":"teacher 1","userId":5,"type":"teacher"}	2020-08-09 22:03:11
+Yz6Q4TdITYK2q7lG5339r04yhCySGIXX	{"cookie":{"originalMaxAge":86400000,"expires":"2020-07-29T06:04:40.541Z","httpOnly":false,"path":"/","sameSite":true},"name":"teacher 1","userId":5,"type":"teacher"}	2020-07-29 11:34:41
 \.
 
 
@@ -438,6 +206,18 @@ f9UJDkuBnAQ_7gE7lt9Y_a11OIaBman6	{"cookie":{"originalMaxAge":1296000000,"expires
 
 COPY public.user_classroom_map (user_id, classroom_id, date_enrolled, date_created) FROM stdin;
 5	2		2020-07-25
+5	7		2020-07-27
+5	8		2020-07-27
+2	8	2020-07-27	
+2	2	2020-07-28	
+8	2	2020-07-28	
+10	2	2020-07-28	
+4	2	2020-07-28	
+5	14		2020-07-28
+2	14	2020-07-28	
+10	14	2020-07-28	
+4	14	2020-07-28	
+7	14	2020-07-28	
 \.
 
 
@@ -463,7 +243,7 @@ COPY public.users (user_id, email, phone, password, date_of_birth, city, state, 
 -- Name: classes_class_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.classes_class_id_seq', 2, true);
+SELECT pg_catalog.setval('public.classes_class_id_seq', 14, true);
 
 
 --
@@ -479,6 +259,14 @@ SELECT pg_catalog.setval('public.users_user_id_seq', 12, true);
 
 ALTER TABLE ONLY public.classrooms
     ADD CONSTRAINT classroom_pkey PRIMARY KEY (classroom_id);
+
+
+--
+-- Name: roles roles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
 
 
 --
@@ -517,6 +305,13 @@ CREATE INDEX classroom_details_index ON public.classrooms USING btree (classroom
 --
 
 CREATE INDEX classroom_map_index ON public.user_classroom_map USING btree (classroom_id);
+
+
+--
+-- Name: user_classroom_pair_unique; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX user_classroom_pair_unique ON public.user_classroom_map USING btree (user_id, classroom_id);
 
 
 --
